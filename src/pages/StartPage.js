0494a003f187b5f057cab98fde5d4947dd2a0ae4
Gr2dd3 <gradde23@gmail.com>
2024-08-06 13:09:58 +0200

    styling linkpage

--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -4,8 +4,15 @@ import '../styling/StartPage.css';
 export default function StartPage(){
     return (
         <>
-        <Link class="segment1" to="/">Start</Link>
-        <Link class="segment1" to="/ThisIsMe">About me</Link>
+        
+        
+        <Link class="segment a2" to="/ThisIsMe">
+        
+        <Link class="home" to="/"><h4>Hem</h4></Link>
+        <img src="C:\Users\gradd\source\repos\myWebPage\mywebpage-fe\src\data\img\Mattias23.jpg" alt="profile" />
+        <h1>Mattias Gradin</h1>
+        </Link>
+        
         </>
     )
 }
