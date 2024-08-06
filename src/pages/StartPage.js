844656ead9c59694daa1a9ce6a28f1589f492a39
Gr2dd3 <gradde23@gmail.com>
2024-08-06 20:29:15 +0200

    added profile pic

--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -1,18 +1,16 @@
 import { Link } from "react-router-dom";
 import '../styling/StartPage.css';
+import portrait from '../img/Mattias23.jpg';
 
 export default function StartPage(){
     return (
         <>
-        
-        
-        <Link class="segment a2" to="/ThisIsMe">
-        
-        <Link class="home" to="/"><h4>Hem</h4></Link>
-        <img src="C:\Users\gradd\source\repos\myWebPage\mywebpage-fe\src\data\img\Mattias23.jpg" alt="profile" />
-        <h1>Mattias Gradin</h1>
-        </Link>
-        
+            <Link class="segment a2" to="/ThisIsMe">
+                <div class="profile-container">
+                    <img src={portrait} class="profile" alt="profile" />
+                </div>
+                <h1>Mattias Gradin</h1>
+            </Link>
         </>
     )
 }
