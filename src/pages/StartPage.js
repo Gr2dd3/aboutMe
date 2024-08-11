2552ad21b25a55275d7f3f1decea83abccf9d3c8
Gr2dd3 <gradde23@gmail.com>
2024-08-11 09:39:42 +0200

    commented unused line

--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -1,6 +1,6 @@
 import AboutMe from '../components/AboutMe';
 import BudgetLink from '../components/BudgetLink';
-import RecipeLink from '../components/RecipeLink';
+// import RecipeLink from '../components/RecipeLink';
 import GameLink from '../components/GameLink';
 import ForumLink from '../components/ForumLink';
 
