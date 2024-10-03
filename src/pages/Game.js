70a640cfa60498ab4be3a9c996224147325d9b24
Gr2dd3 <gradde23@gmail.com>
2024-10-03 22:23:12 +0200

    added styling and text to game.js

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -20,13 +20,20 @@ const GameComponent = () => {
       <div className="text-container">
         <div className="text text-center">
           <h1 className="game-text">Welcome to MAZE RUNNER!</h1>
+          <p className="game-text">Detta är ett labyrint-spel jag skapat med Godot Engine. Här är gränserna 
+            oändliga för vad man kan använda det till. Här kommer några exempel: 3D-modellering, 
+            digitala tvillingar, testa fysik, läroplattformar och utbildningsverktyg, visualisering, 
+            spel och mycket mer!</p>
+            <div className="game-description-box">
           <p className="game-text">Grab the key - Unlock the door</p>
           <p className="game-text">Get to the finish line</p>
           <p className="game-text">Watch out for traps, hidden or not!</p>
           <p className="game-text">Q - Controls</p>
-          <p className="game-text">Klicka på knappen nedan för att starta spelet i ett nytt fönster.</p>
+        </div>
+          <p className="game-text info">Klicka på knappen nedan för att starta spelet i ett nytt fönster.</p>
           <button className="game-start-button" onClick={openGameInNewWindow}>Starta Spelet</button>
         </div>
+
       </div>
     </div>
   );
